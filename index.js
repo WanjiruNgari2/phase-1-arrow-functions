1738201c69b5309a0b03ded5f5444ec725fe8dc5
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-04 00:46:23 +0300

    index,js

---
+++ index.js
@@ -0,0 +1,15 @@
+//divide function
+const divide = (num1, num2) => num1 / num2;
+let num1 = 2000;
+let num2 = 100;
+divide();
+
+
+//function square
+const square = x => x ** 2;
+square(2,);
+
+
+//add function
+const add = (fruit1, fruit2) => fruit1 + fruit2;
+add(3,9);
